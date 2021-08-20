b12774a52bf146512efea46ff7c4b790c14c02b1
Jeremy Dunn <jeremydunn29@gmail.com>
2021-08-20 15:52:58 -0500

    change annoying prettier setting

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
-import {HomeScreen} from './src/screens/Home';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { HomeScreen } from './src/screens/Home';
 
 const client = new ApolloClient({
   uri: 'https://sg-ants-server.herokuapp.com/graphql',
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,11 @@
-import {useQuery} from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import React from 'react';
-import {SafeAreaView, ScrollView, Text, View} from 'react-native';
-import {ALL_ANTS} from '../constants/queries';
-import {GetAllAnts} from '../constants/__generated__/GetAllAnts';
+import { SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { ALL_ANTS } from '../constants/queries';
+import { GetAllAnts } from '../constants/__generated__/GetAllAnts';
 
 export function HomeScreen() {
-  const {data} = useQuery<GetAllAnts>(ALL_ANTS);
+  const { data } = useQuery<GetAllAnts>(ALL_ANTS);
 
   if (data) {
     console.log(data);
